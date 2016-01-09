1b7256a8afee1848c3e4a1e7457a5b398d004b3b
Rossi Guiliani <rossi@guiliani.me>
2016-01-09 04:26:07 -0700

    Josh fucking it up

--- src/lib/audiohack.js
+++ src/lib/audiohack.js
@@ -1,51 +1,42 @@
-    var AudioContext = window.AudioContext || window.webkitAudioContext;
-    var context = new AudioContext();
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var context      = new AudioContext();
 
-function Oscillator (frequency, destination) {
-    var oscillator = this.oscillator = context.createOscillator();
-    var gain = context.createGain();
-    var volume = this.volume = gain.gain;
-
-    oscillator.frequency.value = frequency;
-    volume.value = 0;
-
-    oscillator.connect(gain);
-    gain.connect(destination);
-
-    oscillator.start(0);
-
-    this.start = function() { this.volume.value = 1; };
-    this.stop = function() { this.volume.value = 0; };
-};
-
-function Convolver(input, output) {
-    this.convolver = context.createConvolver(); //node i/o
-    input.connect(this.convolver);
-    this.convolver.connect(output);
-}
-
-function PeriodicWave (osc) {
-    real[0] = 0;
-    imag[0] = 0;
-    real[1] = 1;
-    imag[1] = 0;
-
-    var wave = context.createPeriodicWave(real, imag, {disableNormalization: true});
-    osc.setPeriodicWave(wave);
-}
 chords = [
 //    [448,560,672],
     [440,550,660]
 ];
 
+
+// osc (modified by periodic wave) -> gain -> convolver  -> speakers (aka context.destination)
 chords.forEach(function(freqs) {
     freqs.forEach(function(freq) {
-        var osc = new Oscillator(freq);
-        Convolver(osc, context.destination);
-        PeriodicWave(osc)
+        // osc
+        var oscillator = context.createOscillator();
+        oscillator.frequency.value = freq;
 
-        osc.start();
+        // periodic wave
+        // var real = new Float32Array(2);
+        // var imag = new Float32Array(2);
+        // real[0] = 0;
+        // imag[0] = 0;
+        // real[1] = 1;
+        // imag[1] = 0;
+        // var wave = context.createPeriodicWave(real, imag, {disableNormalization: true});
+        // oscillator.setPeriodicWave(wave);
+
+        // gain
+        var gain     = context.createGain();
+        var volume   = gain.gain;
+        volume.value = 1;
+        oscillator.connect(gain);
+
+        gain.connect(context.destination);
+
+        // // conv
+        // var convolver = context.createConvolver(); 
+        // gain.connect(convolver);
+
+        // // out
+        // convolver.connect(context.destination);
     });
 });
-
-
